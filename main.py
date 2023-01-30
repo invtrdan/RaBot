20c66818c2c5bd39fc79647c654cc0948e902a32
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-01-30 20:51:24 +0000

    added homepage, events page, resident assistants page

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def home():
+  return render_template('homepage.html')
+  
+@app.route('/events', methods=["POST", "GET"])
+def login():
+  return render_template("events.html")
+
+@app.route('/RAs', methods=["POST", "GET"])
+def get_username():
+  return render_template("RAs.html")
+  
 
 
 app.run(host='0.0.0.0', port=81)
